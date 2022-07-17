6250e47d29c1f5450b80810c4676d628d1c45b5f
cc <44751363+zzfn@users.noreply.github.com>
2022-07-17 19:53:47 +0800

    feat: access

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -9,7 +9,7 @@ module.exports = merge(base, {
   mode: "production",
   devtool: "nosources-source-map",
   output: {
-    publicPath: "https://oss-zzf.zzfzzf.com",
+    publicPath: "https://oss-zzf.zzfzzf.com/zeus",
   },
   optimization: {
     runtimeChunk: "single",
