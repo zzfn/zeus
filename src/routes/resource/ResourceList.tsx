0644fcb0f5051197f0bfea892cbaf32802e67793
zzfn <44751363+zzfn@users.noreply.github.com>
2022-08-08 17:47:32 +0800

    feat: 资源管理

---
+++ src/routes/resource/ResourceList.tsx
@@ -0,0 +1,84 @@
+import { useState } from 'react';
+import { articlePage, removeArticle } from 'service/article';
+import ZeusTable from 'components/ZeusTable';
+import { Button, Space, Tag } from 'antd';
+import { Link, useNavigate } from 'react-router-dom';
+import Access from 'components/Access';
+import useAccess from 'hooks/useAccess';
+
+const ResourceList = () => {
+  const access = useAccess();
+  const [params] = useState({});
+  const navigate = useNavigate();
+  const handleDelete = (id: string) => async () => {
+    await removeArticle({ id });
+    navigate(0);
+  };
+  const columns = [
+    {
+      title: '资源名称',
+      dataIndex: 'title',
+      render: (_: string, record: any) => (
+        <a target='_blank' href={`https://zzfzzf.com/article/${record.id}`}>
+          {record.title}
+        </a>
+      ),
+    },
+    {
+      title: '资源类型',
+      dataIndex: 'title',
+      render: (_: string, record: any) => (
+          <a target='_blank' href={`https://zzfzzf.com/article/${record.id}`}>
+            {record.title}
+          </a>
+      ),
+    },
+    {
+      title: '连接地址',
+      dataIndex: 'title',
+      render: (_: string, record: any) => (
+          <a target='_blank' href={`https://zzfzzf.com/article/${record.id}`}>
+            {record.title}
+          </a>
+      ),
+    },
+    {
+      title: '到期时间',
+      dataIndex: 'isRelease',
+      render: (_: boolean) =>
+        _ ? <Tag color='#87d068'>已发布</Tag> : <Tag color='#108ee9'>草稿</Tag>,
+    },
+    {
+      title: '备注',
+      dataIndex: 'orderNum',
+    },
+    {
+      title: '第三方管理链接',
+      dataIndex: 'orderNum',
+    },
+    {
+      title: '操作',
+      dataIndex: 'address',
+      key: 'address',
+      render: (_: string, record: any) => (
+        <Access accessible={access.isAdmin}>
+          <Space>
+            <Link to={`/article/${record.id}`}>编辑</Link>
+            <Button type='text' onClick={handleDelete(record.id)}>删除</Button>
+          </Space>
+        </Access>
+      ),
+    },
+  ];
+
+  return (
+    <>
+      <Link to={`/article/_`}>
+        <Button type='primary'>新增</Button>
+      </Link>
+      <ZeusTable columns={columns} service={articlePage} params={params} />
+    </>
+  );
+};
+
+export default ResourceList;
