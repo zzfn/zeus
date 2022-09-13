80147394a368c57dca2bd518b975af39649edd26
orluna <44751363+zzfn@users.noreply.github.com>
2022-09-13 15:28:58 +0800

    feat: home

--- src/service/home.ts
+++ src/service/home.ts
@@ -2,7 +2,6 @@ import http from 'utils/http';
 
 export async function getHomeOverview(): Promise<any> {
   return http({
-    baseURL: process.env.API_URL,
     url: '/overview/getHomeOverview',
     method: 'get',
   });
