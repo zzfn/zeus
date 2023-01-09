3021fca23130795455cba753e591e06057f36fcc
orluna <44751363+zzfn@users.noreply.github.com>
2023-01-09 14:57:57 +0800

    chore: 登陆页

--- src/routes/Login/index.tsx
+++ src/routes/Login/index.tsx
@@ -25,9 +25,16 @@ const Login = () => {
   }, [user]);
 
   return (
-    <div className='h-screen flex justify-center items-center'>
-      <Form onFinish={onFinish} autoComplete='off'>
+    <div className='h-screen flex flex-col justify-center items-center bg-gradient-to-r from-slate-100 to-gray-300'>
+      <h1>Sign In</h1>
+      <Form
+        className='rounded-lg bg-white/30 p-5 backdrop-opacity-80'
+        layout='vertical'
+        onFinish={onFinish}
+        autoComplete='off'
+      >
         <Form.Item
+          label='USERNAME'
           name='username'
           rules={[{ required: true, message: 'Please input your username!' }]}
         >
@@ -35,6 +42,7 @@ const Login = () => {
         </Form.Item>
 
         <Form.Item
+          label='PASSWORD'
           name='password'
           rules={[{ required: true, message: 'Please input your password!' }]}
         >
@@ -42,11 +50,15 @@ const Login = () => {
         </Form.Item>
 
         <Form.Item>
-          <Button block type='primary' htmlType='submit'>
-            登录
-          </Button>
+          <div className='flex justify-center gap-x-2'>
+            <Button type='primary' htmlType='submit'>
+              登录
+            </Button>
+            <Link to='/register'>
+              <Button>注册</Button>
+            </Link>
+          </div>
         </Form.Item>
-        <Link to='/register'>去注册</Link>
       </Form>
     </div>
   );
